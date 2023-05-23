ff46fde712f78320c8c621df5102f5e56ee338cb
micheleascoli <micheleascoli17@gmail.com>
2023-05-23 18:42:56 -0500

    Atualizando organização dos repositórios.

---
+++ app_jokenpo/android/app/src/main/kotlin/ascolimichele/cursoflutter/com/app_jokenpo/MainActivity.kt
@@ -0,0 +1,6 @@
+package ascolimichele.cursoflutter.com.app_jokenpo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app_jokenpo/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:app_jokenpo/Jogo.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: Jogo(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
