301fd02b7566eedda7b0d7437a65920e92d6a34a
micheleascoli <micheleascoli17@gmail.com>
2023-07-03 17:41:19 -0500

    Salvando dados utilizando

--- app_lista_de_tarefas/lib/Home.dart
+++ app_lista_de_tarefas/lib/Home.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,11 +13,33 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  /* O caminho onde salva os arquivos é diferente para o ios e o android:
+  * ios/listatarefas/dado
+  * android/data/listatarefas/dados
+  * Bibliotecas:
+  * */
 
-  List _listaTarefas = ["Ir ao mercado", "Estudar", "Execício do dia"];
+  List _listaTarefas = [];
+
+  _salvarArquivo() async {
+    final diretorio = await getApplicationDocumentsDirectory();
+    var arquivo = File("${diretorio.path}/dados.json");
+
+    Map<String, dynamic> tarefa = Map();
+
+    tarefa["titulo"] = "Ir ao mercado";
+    tarefa["realizada"] = false;
+    _listaTarefas.add(tarefa);
+
+    String dados = json.encode(_listaTarefas);
+    arquivo.writeAsString(dados);
+  }
 
   @override
   Widget build(BuildContext context) {
+
+    _salvarArquivo;
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Lista de Tarefas"),
