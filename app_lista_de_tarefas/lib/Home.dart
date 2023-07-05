083d98e8a464af73a667f859c558a3023892bc48
micheleascoli <micheleascoli17@gmail.com>
2023-07-05 16:37:11 -0500

    solucionando os erros  de recuperação dos dados.

--- app_lista_de_tarefas/lib/Home.dart
+++ app_lista_de_tarefas/lib/Home.dart
@@ -4,7 +4,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
-
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -14,31 +13,57 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   /* O caminho onde salva os arquivos é diferente para o ios e o android:
-  * ios/listatarefas/dado
-  * android/data/listatarefas/dados
-  * Bibliotecas:
+  * ios/listatarefas/dado  --------- android/data/listatarefas/dados
+  * Biblioteca: path_provider
   * */
 
   List _listaTarefas = [];
 
-  _salvarArquivo() async {
+  Future<File> _getFile() async {
     final diretorio = await getApplicationDocumentsDirectory();
-    var arquivo = File("${diretorio.path}/dados.json");
+    return File("${diretorio.path}/dados.json");
+  }
 
+  _salvarArquivo() async {
+    var arquivo = await _getFile();
+
+    //Criar dados
     Map<String, dynamic> tarefa = Map();
 
     tarefa["titulo"] = "Ir ao mercado";
-    tarefa["realizada"] = false;
+    tarefa["realizada"] = true;
+
     _listaTarefas.add(tarefa);
 
     String dados = json.encode(_listaTarefas);
     arquivo.writeAsString(dados);
   }
 
+  _lerArquivo() async {
+    try{
+      final arquivo = await _getFile();
+      return arquivo.readAsString();
+    } catch(e) {
+      return null;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _lerArquivo().then((dados){
+      setState(() {
+        _listaTarefas = json.decode(dados);
+      });
+    } );
+  }
+
   @override
   Widget build(BuildContext context) {
 
-    _salvarArquivo;
+    //_salvarArquivo();
+   //print("itens: "+_listaTarefas.toString());
 
     return Scaffold(
       appBar: AppBar(
@@ -60,6 +85,7 @@ class _HomeState extends State<Home> {
                       labelText: "Digite sua tarefa"
                     ),
                     onChanged: (text){
+                      _listaTarefas;
 
                     },
                   ),
@@ -88,7 +114,7 @@ class _HomeState extends State<Home> {
                itemCount: _listaTarefas.length,
               itemBuilder: (context, index){
                  return ListTile(
-                   title: Text(_listaTarefas[index]),
+                   title: Text(_listaTarefas[index]['titulo']),
                  );
               },
             ),
